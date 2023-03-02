b3a828018d06158dd8e51c10c690b86eef9f0722
reinexxism <lucy0805@naver.com>
2023-03-02 12:44:27 +0900

    need to finish js

---
+++ 2. Counter/main.js
@@ -0,0 +1,13 @@
+const increaseBtn = document.getElementById("increase");
+const decreaseBtn = document.getElementById("decrease");
+const resetBtn = document.getElementById("reset");
+
+function numberUp() {}
+
+function numberDown() {}
+
+function backToZero() {}
+
+increaseBtn.addEventListener("click", numberUp);
+decreaseBtn.addEventListener("click", numberDown);
+resetBtn.addEventListener("click", backToZero);
