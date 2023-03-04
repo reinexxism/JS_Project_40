7bf9be98c82c2e19c3d05620b09c104a07516553
reinexxism <lucy0805@naver.com>
2023-03-04 10:28:30 +0900

    need to add function

---
+++ 2. Counter/README.md
@@ -0,0 +1,19 @@
+## 1. Counter
+
+양수인지, 음수인지에 따라 색이 바뀌는 카운터입니다.
+
+이 프로젝트를 통해 DOM을 더 연습할 수 있으며, 뽀모도로 시계와 같은 다른 프로젝트에서도 이 간단한 카운터를 사용할 수 있습니다.
+
+## 2. Main Idea
+
+1. document.querySelectorAll() / 사용X
+
+2. forEach() / 사용X
+
+3. addEventListener()
+
+4. currentTarget 속성 / 사용X
+
+5. classList / 사용X
+
+6. textContent
--- 2. Counter/main.js
+++ 2. Counter/main.js
@@ -1,12 +1,22 @@
 const increaseBtn = document.getElementById("increase");
 const decreaseBtn = document.getElementById("decrease");
 const resetBtn = document.getElementById("reset");
+const showNumber = document.getElementById("showNumber");
+let count = 0;
 
-function numberUp() {}
+function numberUp() {
+  count++;
+  showNumber.textContent = count;
+}
 
-function numberDown() {}
+function numberDown() {
+  count--;
+  showNumber.textContent = count;
+}
 
-function backToZero() {}
+function backToZero() {
+  showNumber.innerText = 0;
+}
 
 increaseBtn.addEventListener("click", numberUp);
 decreaseBtn.addEventListener("click", numberDown);
